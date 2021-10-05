741601af069babfde615782fd8c8e654d782ae6a
Rodrigo Bonatto <rwbonatto@gmail.com>
2021-10-05 14:39:41 -0300

    added styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-export function App() {
+import styled from 'styled-components'
+import { GlobalStyle } from './styles/global'
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #4f20bb;
+`
+export default function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <Title>Welcome. {11+20}</Title>
+      <GlobalStyle />
     </div>
   );
 }
