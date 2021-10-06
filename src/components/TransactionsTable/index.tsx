407a177944e52d180b5a544ecab50544bf20c6ef
Rodrigo Bonatto <rwbonatto@gmail.com>
2021-10-05 23:29:03 -0300

    replacing fetch by axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
+import { api } from '../../services/api';
 import { Container } from './styles'
 
 export function TransactionsTable() {
   useEffect(() => {
-    fetch('http://192.168.1.117:3000/api/transactions')
-      .then(response => response.json())
-      .then(data => console.log(data))
+    api.get('/transactions')
+      .then(response => console.log(response.data))
   }, []);
 
   return (
