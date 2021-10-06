cfb5f2bec2eca5c1131c3e311d0b5dd765c79279
Rodrigo Bonatto <rwbonatto@gmail.com>
2021-10-05 22:47:23 -0300

    featuring data grid

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -11,21 +11,21 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>R$12.000,00</strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Entradas" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>R$6.500,00</strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Entradas" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>R$5.500,00</strong>
       </div>
     </Container>
   )
